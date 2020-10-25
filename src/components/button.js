9917a8be7828e829cd5510c9f498e621cbcfca54
Christian Huelsemeyer <christian.huelsemeyer@gmx.net>
2020-10-25 16:56:41 +0100

    add step 2 hidden

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { Platform, StyleSheet, TextInput, Text, Linking, View, TouchableOpacity} from 'react-native';
+import { Platform, StyleSheet, TextInput, Text, Linking, View, Pressable} from 'react-native';
 
 class OurButton extends Component {
 constructor(props) {
@@ -9,17 +9,36 @@ constructor(props) {
   render() {
       return (
         <View style={{margin: 15}}>
-        <TouchableOpacity onPress={() => this.props.onPress()}>
+        <Pressable onPress={() => this.props.onPress()}>
         <View style={styles.button}>
         <Text style={{color: 'white', fontSize: 15, fontWeight: '800'}}>
         {this.props.label}
         </Text>
         </View>
-        </TouchableOpacity>
+        </Pressable>
         </View>
       );
     }
 }
+
+class OurButtonPlaceholder extends Component {
+    constructor(props) {
+        super(props)
+    }
+      render() {
+          return (
+            <View style={{margin: 15}}>
+            <Pressable onPress={() => this.props.onPress()} disabled='true'>
+            <View style={styles.button}>
+            <Text style={{color: 'white', fontSize: 15, fontWeight: '800'}}>
+            {this.props.label}
+            </Text>
+            </View>
+            </Pressable>
+            </View>
+          );
+        }
+    }
 
 const styles = StyleSheet.create({
     button: {
@@ -36,5 +55,6 @@ const styles = StyleSheet.create({
   });
 
 export default OurButton
+export {OurButton, OurButtonPlaceholder}
 
 
--- src/components/facilitators.js
+++ src/components/facilitators.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react'
 import { Platform, TouchableOpacity, StyleSheet, Text, Linking, View, Button } from 'react-native';
-import OurButton from './button';
+import { OurButton, OurButtonPlaceholder} from './button';
 import OurText from './text';
 import {isMobile} from 'react-device-detect';
 
@@ -30,7 +30,7 @@ class Facilitators extends React.Component {
   render() {
     if(this.state.stage == 'none') {
       return (
-        <View style={styles.padding}>
+      <View style={styles.padding}>
         <View style={{ marginTop: 10}}>
         <OurText style={styles.titleText}>Schritt 1</OurText>
         </View>
@@ -39,7 +39,20 @@ class Facilitators extends React.Component {
         </View>
         <OurButton onPress={() => this.invite_whatsapp('https://wa.me/?text=Hey%2C+du+solltest+mal+wieder+neue+Leute+kennenlernen+%21+Ich+veranstalte+eine+kleine+Party+auf+WhatsApp+%3A%29+Meld+Dich+mal+an%3A+https%3A%2F%2Fgetustogether.netlify.app%2Fb', 'besties_invited')} label="Freunde einladen"/>
         <StatusBar style="auto" />
+
+        <View style={[styles.hide]}>
+        <View style={{ marginTop: 10}}>
+        <OurText style={styles.titleText}>Schritt 2</OurText>
+        </View>
+        <View style={{ marginTop: 10}}>
+        <OurText>Lade weitere Freunde und Bekannte ein, die deine besten Freunde kennenlernen sollten:</OurText>
+        </View>
+        <OurButtonPlaceholder onPress={() => this.invite_whatsapp('https://wa.me/?text=Na%2C+hoffe%2C+es+geht+dir+gut%21+%3A%29+Ich+schmei%C3%9F+ne+kleine+virtuelle+Party+zum+Kennenlernen+auf+WhatsApp%2C+hast+du+Lust+ein+paar+neue+Leute+kennenzulernen%3F+https%3A%2F%2Fgetustogether.netlify.com%2Ff', 'friends_invited')} label="Freunde einladen"/>
+        <StatusBar style="auto" />
+        </View>
+
       </View>
+
       );
     }
     if(this.state.stage == 'besties_invited') {
@@ -75,6 +88,9 @@ class Facilitators extends React.Component {
 const styles = StyleSheet.create({
   padding: {
     padding: 20
+  },
+  hide: {
+    opacity: 0.5
   },
   titleText: {
     fontSize: 20,
