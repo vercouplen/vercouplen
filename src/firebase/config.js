30ad67baf546bb28de038b2da77d1b78352d73d0
Christian Huelsemeyer <christian.huelsemeyer@gmx.net>
2020-10-25 23:53:04 +0100

    remove measurement ID

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -14,7 +14,6 @@ var firebaseConfig = {
     storageBucket: "vercouplen.appspot.com",
     messagingSenderId: "648584206579",
     appId: "1:648584206579:web:c482cac630ad80b348ff86",
-    measurementId: "G-73WL1HHVE9",
   };
 
 firebase.initializeApp(firebaseConfig);
